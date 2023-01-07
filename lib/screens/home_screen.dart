b575d8989d5cb9a3fa6344be0611abfb019896d5
Muhammad Awais Yaseen <85793305+muhammadawaisyaseen@users.noreply.github.com>
2023-01-07 22:40:24 +0500

    error fixing

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:freecodecampcourse/firebase_options.dart';
+import 'package:freecodecampcourse/screens/email_verification_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,11 +20,21 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              // user?.emailVerified ?? false) means that if user is verified return true then false
+              if (user?.emailVerified ?? false) {
+                print('You are a verified user');
+              } else {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EmailVerificationView(),
+                    ));
+              }
               return Text('Done');
             default:
               return Text('Loading........');
           }
-          
         },
       ),
     );
